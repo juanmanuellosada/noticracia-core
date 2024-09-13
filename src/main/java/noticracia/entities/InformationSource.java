68dc88a82599be35c6fbd67e5f53de53eac8d853
donatofranco <francoedonato@gmail.com>
2024-09-13 09:46:11 -0300

    Add SuppressWarnings

--- src/main/java/noticracia/entities/InformationSource.java
+++ src/main/java/noticracia/entities/InformationSource.java
@@ -2,6 +2,7 @@ package noticracia.entities;
 
 import java.util.Observable;
 
+@SuppressWarnings("deprecation")
 public abstract class InformationSource extends Observable {
 
     //Este notifica cuando termina
