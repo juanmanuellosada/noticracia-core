cb6b9accc24d3a0e5dd6302f788c06b001e9630a
donatofranco <francoedonato@gmail.com>
2024-09-17 22:25:09 -0300

    Replace 'postProcess' -> 'refresh'

--- src/main/java/noticracia/entities/InformationSource.java
+++ src/main/java/noticracia/entities/InformationSource.java
@@ -5,7 +5,6 @@ import noticracia.services.information.manager.InformationSourceBroker;
 import java.util.Map;
 
 public abstract class InformationSource {
-
     private final InformationSourceBroker informationSourceBroker;
 
     public InformationSource(InformationSourceBroker informationSourceBroker) {
@@ -13,14 +12,14 @@ public abstract class InformationSource {
     }
 
     public void startInformationCollection(String searchCriteria){
-        postProcess(process(searchCriteria));
+        refresh(process(searchCriteria));
     }
 
     public abstract Map<String, String> process(String query);
 
     public abstract String getName();
 
-    private void postProcess(Map<String, String> information) {
+    private void refresh(Map<String, String> information) {
         informationSourceBroker.refreshInformation(information);
     }
 }
