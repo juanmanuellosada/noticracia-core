413f57aea99a9c17aea916c28c0832a84f4c3bc2
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-09-18 21:02:39 -0300

    Mejora de codigo

--- src/main/java/noticracia/services/information/broker/InformationSourceBroker.java
+++ src/main/java/noticracia/services/information/broker/InformationSourceBroker.java
@@ -17,14 +17,16 @@ public class InformationSourceBroker {
     }
 
     public boolean startInformationCollection(InformationSource informationSource, String searchCriteria) {
-        if (Objects.isNull(currentInformationSource) ||
-                !currentInformationSource.getName().equals(informationSource.getName())) {
-            if (Objects.nonNull(currentInformationSource)) {
-                currentInformationSource.stopProcess();
-            }
+        if (isNotCurrentInformationSource(informationSource)) {
+            currentInformationSource.stopProcess();
             currentInformationSource = informationSource;
         }
         return currentInformationSource.startProcess(searchCriteria);
+    }
+
+    public boolean isNotCurrentInformationSource(InformationSource informationSource) {
+        return Objects.nonNull(currentInformationSource) &&
+                !currentInformationSource.getName().equals(informationSource.getName());
     }
 
     public boolean refreshInformation(Map<String, String> information) {
