20715b1a02e6eafa918bcc6f402363f3117ab48f
donatofranco <francoedonato@gmail.com>
2024-09-19 22:00:47 -0300

    Update regex

--- src/main/java/noticracia/services/worldCloud/WordCloud.java
+++ src/main/java/noticracia/services/worldCloud/WordCloud.java
@@ -7,7 +7,7 @@ public class WordCloud {
 
     public static Map<String, Integer> generate(Map<String, String> information) {
         return information.values().stream()
-                .flatMap(text -> Arrays.stream(text.toLowerCase().split("[ .,;\\n]+")))
+                .flatMap(text -> Arrays.stream(text.toLowerCase().split("[ .,;\\n\"'“”‘’]+")))
                 .filter((word) -> word.length() > 3)
                 .collect(Collectors.toMap(word -> word, word -> 1, Integer::sum));
     }
