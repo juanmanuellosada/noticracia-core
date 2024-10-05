cefec0c19ea2f083717a4b26a7c01c4f74ef2353
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-10-05 20:09:55 -0300

    Cambio return del Special case

--- src/main/java/noticracia/entities/InformationSourceNull.java
+++ src/main/java/noticracia/entities/InformationSourceNull.java
@@ -1,12 +1,13 @@
 package noticracia.entities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class InformationSourceNull implements InformationSource {
 
     @Override
     public Map<String, String> startSearch(String searchCriteria) {
-        return null;
+        return new HashMap<>();
     }
 
     @Override
